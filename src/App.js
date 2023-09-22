efd9a118327ff072904fefe3d7b4692529b40147
rubaiyat12 <rubaiyatraha12@gmail.com>
2023-09-21 22:48:14 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState } from "react";
+import Landing from "./scenes/Landing";
+import DotGroup from "./scenes/DotGroup";
+import MySkills from "./scenes/MySkills";
+import Projects from "./scenes/Projects";
+import Testimonials from "./scenes/Testimonials";
+import Contact from "./scenes/Contact";
+import Footer from "./scenes/Footer";
+import useMediaQuery from "./hooks/useMediaQuery";
+import Navbar from "./scenes/Navbar";
+import LineGradient from "./components/LineGradient";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState("home");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage("home");
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app bg-deep-blue">
+
+      <Navbar
+      isTopOfPage={isTopOfPage}
+       selectedPage={selectedPage}
+       setSelectedPage={setSelectedPage} />
+
+<div className="w-5/6 mx-auto md:h-full ">
+       {
+        isAboveMediumScreens && (
+          <DotGroup
+
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+
+           />
+        )
+       }
+       <Landing setSelectedPage={setSelectedPage} />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:full">
+        <MySkills />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto ">
+        <Projects />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full ">
+        <Testimonials />
+        
+      </div>
+     
+      
+
+
+      
+      <div className="w-5/6 mx-auto  md:h-full  ">
+        <Contact />
+        
+      </div>
+
+      <div className="mt-50">
+      <Footer  />
+
+      </div>
+      
+      
+        
+      
+     
+
+      
     </div>
   );
 }
---
+++ src/components/SocialMediaicons.jsx
@@ -0,0 +1,37 @@
+import facebook from '../assets/facebook.png' ;
+import instagram from '../assets/instagram.png' ;
+import linkedin from '../assets/linkedin.png' ;
+
+const SocialMediaIcons = () => {
+    return (
+      <div className="flex justify-center md:justify-start my-10 gap-7">
+        <a
+          className="hover:opacity-50 transition duration-500"
+          href="https://www.linkedin.com/in/rubaiyat-razin-raha/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img alt="linkedin-link" src={linkedin} />
+        </a>
+       
+        <a
+          className="hover:opacity-50 transition duration-500"
+          href="https://www.facebook.com/profile.php?id=100004530016055"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img alt="facebook-link" src={facebook} />
+        </a>
+        <a
+          className="hover:opacity-50 transition duration-500"
+          href="https://www.instagram.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img alt="instagram-link" src={instagram} />
+        </a>
+      </div>
+    );
+  };
+  
+  export default SocialMediaIcons;
